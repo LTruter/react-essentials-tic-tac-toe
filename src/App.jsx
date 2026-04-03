e43e1d67ff3f8431a33e3e6afe842b9c59b753ed
LTruter <lariotruter28@gmail.com>
2026-04-03 15:53:10 +0200

    Improve components' readability

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,12 @@ import Log from "./components/Log";
 import { WINNING_COMBINATIONS } from "./winning-combinations";
 import GameOver from "./components/GameOver";
 
-const initialGameBoard = [
+const PLAYERS = {
+  X: "Player 1",
+  O: "Player 2",
+};
+
+const INITIAL_GAME_BOARD = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
@@ -24,14 +29,9 @@ function deriveActivePlayer(gameTurns) {
   return currentPlayer;
 }
 
-function App() {
-  const [gameTurns, setGameTurns] = useState([]);
-  const [players, setPlayers] = useState({ X: "Player 1", O: "Player 2" });
-
-  const activePlayer = deriveActivePlayer(gameTurns);
-
-  // Create a deep copy of initialGameBoard
-  let gameBoard = [...initialGameBoard.map((array) => [...array])];
+function deriveGameBoard(gameTurns) {
+  // Create a deep copy
+  let gameBoard = [...INITIAL_GAME_BOARD.map((array) => [...array])];
 
   // Update gameboard to reflect the player's turn
   for (const turn of gameTurns) {
@@ -41,6 +41,10 @@ function App() {
     gameBoard[row][col] = player;
   }
 
+  return gameBoard;
+}
+
+function deriveWinner(gameBoard, players) {
   let winner;
 
   // Loop through the nested arrays
@@ -61,7 +65,16 @@ function App() {
       winner = players[firstSquareSymbol];
     }
   }
+  return winner;
+}
 
+function App() {
+  const [gameTurns, setGameTurns] = useState([]);
+  const [players, setPlayers] = useState(PLAYERS);
+
+  const activePlayer = deriveActivePlayer(gameTurns);
+  const gameBoard = deriveGameBoard(gameTurns);
+  const winner = deriveWinner(gameBoard, players);
   // Checks if there was a draw
   const hasDraw = gameTurns.length === 9 && !winner;
 
@@ -96,13 +109,13 @@ function App() {
       <div id="game-container">
         <ol id="players" className="highlight-player">
           <Player
-            initialName="Player 1"
+            initialName={PLAYERS.X}
             symbol="X"
             isActive={activePlayer === "X"}
             onPlayerNameChange={handlePlayerNameChanges}
           />
           <Player
-            initialName="Player 2"
+            initialName={PLAYERS.O}
             symbol="O"
             isActive={activePlayer === "O"}
             onPlayerNameChange={handlePlayerNameChanges}
@@ -113,7 +126,7 @@ function App() {
         )}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
-      <Log turns={gameTurns} />
+      <Log turns={gameTurns} playerNames={players} />
     </main>
   );
 }
--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,9 +1,10 @@
-function Log({ turns }) {
+function Log({ turns, playerNames }) {
   return (
     <ol id="log">
       {turns.map(({ square, player }) => (
         <li key={`${square.row}${square.col}`}>
-          {player} selected Row: {square.row + 1}, Column: {square.col + 1}
+          {playerNames[player]} selected Row: {square.row + 1}, Column:{" "}
+          {square.col + 1}
         </li>
       ))}
     </ol>
